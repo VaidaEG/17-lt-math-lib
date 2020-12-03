fe2b59c07a1f5b51f3782e568db984f2eb2b4ca1
VaidaEG <grausliene.vaida@gmail.com>
2020-12-03 13:33:36 +0200

    Matematika library

---
+++ js/components/matematika/matematika.js
@@ -0,0 +1,6 @@
+import { sumavimas } from './sumavimas.js';
+import { atimtis } from './atimtis.js';
+import { daugyba } from './daugyba.js';
+import { dalyba } from './dalyba.js';
+
+export { sumavimas, atimtis, daugyba, dalyba }
---
+++ js/main.js
@@ -0,0 +1,8 @@
+import * as Matematika  from './components/matematika/matematika.js';
+
+console.log(Matematika);
+
+console.log(Matematika.sumavimas(5, 48))
+console.log(Matematika.atimtis(10, 6));
+console.log(Matematika.dalyba(8, 2));
+console.log(Matematika.daugyba(10, 8));
