c49a48cb2ce7a1ed858a514d6996b36fef479794
VaidaEG <grausliene.vaida@gmail.com>
2020-12-07 13:41:50 +0200

    Kvadratu

--- js/main.js
+++ js/main.js
@@ -6,3 +6,4 @@ console.log(Matematika.sumavimas(5, 48))
 console.log(Matematika.atimtis(10, 6));
 console.log(Matematika.dalyba(8, 2));
 console.log(Matematika.daugyba(10, 8));
+console.log(Matematika.kvadratu(5));
